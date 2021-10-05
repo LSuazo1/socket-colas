6636b5e9faaad29fadbbd487aaba50dce648bc36
LSuazo1 <luissuazo80@yahoo.com>
2021-10-05 13:06:53 -0600

    Pantalla escritorio terminada

--- public/js/escritorio.js
+++ public/js/escritorio.js
@@ -1,3 +1,52 @@
+//Referencias HTML
+const lblEscritorio=document.querySelector('h1');
+const btnAtender=document.querySelector('button');
+const lblTicket=document.querySelector('small');
+const divAlerta=document.querySelector('.alert');
+const searchParams=new URLSearchParams(window.location.search);
+
+if (!searchParams.has('escritorio')) {
+    window.location='index.html';
+    throw new Error('El escritorio es oblicatorio');
+}
+
+const escritorio = searchParams.get('escritorio');
+lblEscritorio.innerText="Escritorio "+escritorio;
+
+divAlerta.style.display='none';
+
+const socket = io();
 
 
-console.log('Escritorio HTML');
+
+socket.on('connect', () => {
+
+    btnAtender.disabled=false;
+   
+});
+
+socket.on('disconnect', () => {
+    
+    btnAtender.disabled=true;
+
+});
+
+socket.on('ultimo-ticket',(ticket)=>{
+  //  lblNuevoTicket.innerText = "Ticket "+ ticket;
+   });
+
+btnAtender.addEventListener( 'click', () => {
+    
+
+
+  socket.emit('atender-ticket',{escritorio},({ok,ticket,msg})=>{
+    if(!ok){
+        lblTicket.innerText=`Nadie`
+        return divAlerta.style.display= '';
+    }
+    console.log(ticket.numero);
+    lblTicket.innerText=`Ticket ${ticket.numero}`;
+
+
+  });
+});
--- sockets/controller.js
+++ sockets/controller.js
@@ -1,31 +1,51 @@
-const TicketControl=require('../models/ticket-control');
+const TicketControl = require('../models/ticket-control');
 
-const ticketControl=new TicketControl();
+const ticketControl = new TicketControl();
 
 
 const socketController = (socket) => {
-    //socket.on('disconnect', () => {
-      //  console.log('Cliente desconectado', socket.id );
-    //});
+  //socket.on('disconnect', () => {
+  //  console.log('Cliente desconectado', socket.id );
+  //});
 
-    socket.emit('ultimo-ticket',ticketControl.ultimo);
-
-
-    socket.on('siguiente-ticket', ( payload, callback ) => {
-        
-      let siguiente=ticketControl.siguiente();
-      callback(siguiente);
-
-      //Notificar que hay un nuevo ticket
+  socket.emit('ultimo-ticket', ticketControl.ultimo);
 
 
-    })
+  socket.on('siguiente-ticket', (payload, callback) => {
+
+    let siguiente = ticketControl.siguiente();
+    callback(siguiente);
+
+    //Notificar que hay un nuevo ticket
+  });
+
+  socket.on('atender-ticket', ({ escritorio }, callback) => {
+    if (!escritorio) {
+      return callback({
+        ok: false,
+        msg: 'Es escritorio es obligatorio'
+      });
+    }
+    const ticket = ticketControl.atenderTicket(escritorio);
+    if (!ticket) {
+      callback({
+        ok: false,
+        msg: 'Ya no hay ticket pendientes'
+      });
+    } else {
+      callback({
+        ok: true,
+        ticket
+      });
+    }
+
+  });
 
 }
 
 
 
 module.exports = {
-    socketController
+  socketController
 }
 
